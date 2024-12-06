65934e7f65602c6d9ca5522477ae42bdc1cd936f
Troels_Dankert <132554964+TroelsD@users.noreply.github.com>
2024-12-06 12:51:53 +0100

    landingpageskitse

--- dry-react/src/Pages/Home.jsx
+++ dry-react/src/Pages/Home.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
+import config from '../../config.jsx';
+import HomeGearCard from "./HomeGearCard.jsx";
 
 function Home() {
+    const [musicGear, setMusicGear] = useState([]);
+
+    useEffect(() => {
+        const fetchMusicGear = async () => {
+            try {
+                const response = await fetch(`${config.apiBaseUrl}/api/MusicGear`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setMusicGear(Array.isArray(data.items) ? data.items : []);
+            } catch (error) {
+                console.error('Error fetching music gear:', error);
+            }
+        };
+        fetchMusicGear();
+    }, []);
+
     return (
         <div className="home-container">
-
-            <div>
-                <h1>Hej Troels</h1>
+            <h1>Hej Troels</h1>
+            <div className="homegear-list">
+                {musicGear.map((item) => (
+                    <HomeGearCard key={item.id} item={item} />
+                ))}
             </div>
-
         </div>
     );
 }
